06a114fe1ef2e4d5a2abe4948570d53386de502c
EwarJames <ewaarjames@gmail.com>
2021-02-18 18:25:45 +0000

    Base Model

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,7 @@ class BaseModel:
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
             for key, value in kwargs.items():
-                if key == "created_at" or k == "updated_at":
+                if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(value, d_form)
                 else:
                     self.__dict__[key] = value
